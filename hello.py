4cff8d5b92c33cd74ca059f6def6c54307af78bd
shyboys <bignerd@163.com>
2018-11-07 09:36:12 +0800

    aaaaa

--- hello.py
+++ hello.py
@@ -3,10 +3,14 @@ import web
 urls = (
         '/(.*)', 'hello'
     )
+
+render = web.template.render('templates')
+db = web.database(dbn='mysql', user='root', pw='', db='myblog')
 app = web.application(urls, globals())
 
 class hello:
     def GET(self, name):
+        test = db.select('test')
         if not name:
             name = 'World'
         return 'Hello, ' + name + '!'
